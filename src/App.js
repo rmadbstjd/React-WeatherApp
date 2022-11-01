b7ae57edcec4583f33d73b8f4d8b686841354e73
rmadbstjd <rmadbstjd@naver.com>
2022-11-01 20:19:05 +0900

    현재 위치를 기반으로 날씨의 상태를 가져오는 함수 작성

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 function App() {
+  const [lat, setLat] = useState();
+  const [lon, setLon] = useState();
   const getCurrentLocation = () => {
+    console.log("1");
     navigator.geolocation.getCurrentPosition(showPosition);
+    console.log("3");
   };
   const showPosition = (position) => {
-    let lat = position.coords.latitude;
-    let lon = position.coords.longitude;
-    console.log("현재 위치", lat, lon);
+    console.log("2");
+
+    setLat(() => position.coords.latitude);
+    setLon(() => position.coords.longitude);
+  };
+  const getCurrentWeather = (lat, lon) => {
+    const API_KEY = "f224b4bceab7f1da7f43966dc18bfa6d";
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        const temp = data.main.temp;
+        const weathers = data.weather[data.weather.length - 1];
+        console.log(temp, weathers);
+      });
   };
   useEffect(() => {
     getCurrentLocation();
   }, []);
+
+  useEffect(() => {
+    if (lat) {
+      getCurrentWeather(lat, lon);
+    }
+  }, [lat]);
   return <div></div>;
 }
 
