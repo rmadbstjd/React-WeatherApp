10d6dc62b2f384736be634eb4f163977867bdccd
rmadbstjd <rmadbstjd@naver.com>
2022-11-01 19:49:27 +0900

    자바스크립트의 navigator 객체를 활용하여 현재 위치(위도,경도)를 받아오는 함수 작성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  };
+  const showPosition = (position) => {
+    let lat = position.coords.latitude;
+    let lon = position.coords.longitude;
+    console.log("현재 위치", lat, lon);
+  };
+  useEffect(() => {
+    getCurrentLocation();
+  }, []);
   return <div></div>;
 }
 
